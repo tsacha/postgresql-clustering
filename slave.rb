6d04791c4227e5feaa33bd70cb2e87b1114861ee
Sacha Trémoureux <sacha@tremoureux.fr>
2013-03-09 21:34:10 +0100

    Remove from repository auto-generated configuration files, slave DB configuration script added.

---
+++ slave.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+load "#{File.dirname(__FILE__)}/const.rb"
+require 'fileutils'
+
+STDOUT.sync = true
+
+# Configuration de la machine maître
+
+Dir.chdir(PSQL_FOLDER)
+
+FileUtils.rm_r("./data") if FileTest.directory?('./data/')
+system("su - #{PSQL_USER} -c '#{PSQL_FOLDER}/bin/initdb #{PSQL_FOLDER}/data/'")
+FileUtils.mkdir_p(PSQL_FOLDER+'/'+HOST_MASTER) if not FileTest.directory?(PSQL_FOLDER+'/'+HOST_MASTER)
